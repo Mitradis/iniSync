e813c12ea8a4552212b7eb414204157fe6e66d14
Vitaliy <mitradis@gmail.com>
2022-05-23 17:44:19 +0300

    ReUpload

---
+++ iniSync/FormMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace iniSync
+{
+    public partial class FormMain : Form
+    {
+        string lastFolder1 = null;
+        string lastFolder2 = null;
+        string file1 = null;
+        string file2 = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            file1 = null;
+            if (lastFolder1 != null)
+            {
+                if (Directory.Exists(lastFolder1))
+                {
+                    openFileDialog1.InitialDirectory = lastFolder1;
+                }
+            }
+            else
+            {
+                openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                file1 = openFileDialog1.FileName;
+                lastFolder1 = Path.GetDirectoryName(openFileDialog1.FileName);
+                textBox1.Text = file1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            file2 = null;
+            if (lastFolder2 != null)
+            {
+                if (Directory.Exists(lastFolder2))
+                {
+                    openFileDialog2.InitialDirectory = lastFolder2;
+                }
+            }
+            else
+            {
+                openFileDialog2.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                file2 = openFileDialog2.FileName;
+                lastFolder2 = Path.GetDirectoryName(openFileDialog2.FileName);
+                textBox2.Text = file2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(file1) && File.Exists(file2))
+            {
+                string currentSection = null;
+                List<string> cacheFile = new List<string>();
+                cacheFile.AddRange(File.ReadAllLines(file1));
+                foreach (string line in cacheFile)
+                {
+                    if (line.StartsWith("[") && line.EndsWith("]"))
+                    {
+                        currentSection = line.Replace("[", "").Replace("]", "");
+                    }
+                    if (line.Contains("="))
+                    {
+                        if (FuncParser.keyExists(file2, currentSection, line.Remove(line.IndexOf('='))))
+                        {
+                            FuncParser.iniWrite(file2, currentSection, line.Remove(line.IndexOf('=')), line.Remove(0, line.IndexOf('=') + 1));
+                        }
+                    }
+                }
+                cacheFile = null;
+            }
+        }
+    }
+}
---
+++ iniSync/FuncParser.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iniSync
+{
+    class FuncParser
+    {
+        static List<string> cacheFile = new List<string>();
+        static int startIndex = -1;
+        static int enbIndex = -1;
+        static int lineIndex = -1;
+        static bool blockClearEK = false;
+        static bool blockClearSR = false;
+
+        public static bool keyExists(string path, string section, string key)
+        {
+            startIndex = -1;
+            enbIndex = -1;
+            lineIndex = -1;
+            bool findSection = false;
+            bool findKey = false;
+            if (File.Exists(path))
+            {
+                cacheFile = new List<string>(File.ReadAllLines(path));
+                for (int i = 0; i < cacheFile.Count; i++)
+                {
+                    if (!findSection && cacheFile[i] == "[" + section + "]")
+                    {
+                        findSection = true;
+                        startIndex = i;
+                        enbIndex = i;
+                    }
+                    else if (findSection && cacheFile[i].StartsWith("[") && cacheFile[i].EndsWith("]"))
+                    {
+                        break;
+                    }
+                    else if (findSection && cacheFile[i].Length > 0)
+                    {
+                        if (cacheFile[i].StartsWith(key + "="))
+                        {
+                            findKey = true;
+                            lineIndex = i;
+                            break;
+                        }
+                        else
+                        {
+                            enbIndex = i;
+                        }
+                    }
+                }
+            }
+            if (!blockClearEK)
+            {
+                cacheFile.Clear();
+            }
+            return findKey;
+        }
+
+        public static string stringRead(string path, string section, string key)
+        {
+            string outString = null;
+            blockClearEK = true;
+            if (keyExists(path, section, key))
+            {
+                outString = cacheFile[lineIndex].Remove(0, cacheFile[lineIndex].IndexOf('=') + 1);
+                if (outString.Length == 0)
+                {
+                    outString = null;
+                }
+            }
+            blockClearEK = false;
+            if (!blockClearSR)
+            {
+                cacheFile.Clear();
+            }
+            return outString;
+        }
+
+        public static void iniWrite(string path, string section, string key, string value)
+        {
+            bool readyToWrite = false;
+            blockClearSR = true;
+            string line = stringRead(path, section, key);
+            blockClearSR = false;
+            if (lineIndex != -1)
+            {
+                if (line != value)
+                {
+                    cacheFile[lineIndex] = key + "=" + value;
+                    readyToWrite = true;
+                }
+            }
+            else
+            {
+                if (startIndex != -1 && enbIndex != -1)
+                {
+                    cacheFile[enbIndex] += Environment.NewLine + key + "=" + value;
+                    readyToWrite = true;
+                }
+                else
+                {
+                    File.AppendAllText(path, Environment.NewLine + "[" + section + "]" + Environment.NewLine + key + "=" + value + Environment.NewLine);
+                }
+            }
+            if (readyToWrite)
+            {
+                try
+                {
+                    File.WriteAllLines(path, cacheFile, new UTF8Encoding(false));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось записать файл: " + path + Environment.NewLine + ex.Message);
+                }
+            }
+            cacheFile.Clear();
+        }
+    }
+}
